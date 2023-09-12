73f2e610db9d4a9a1963cbff8fd5c665941eda43
s25093 <s25093@pjwstk.edu.pl>
2023-09-12 10:51:05 +0200

    got the search results page to work but the search function needs changing

--- book_app/src/main/java/com/example/book_app/controller/page/SearchController.java
+++ book_app/src/main/java/com/example/book_app/controller/page/SearchController.java
@@ -1,5 +1,8 @@
 package com.example.book_app.controller.page;
 
+import com.example.book_app.dto.ApiResultsDto;
+import com.example.book_app.dto.BookAndUsersDto;
+import com.example.book_app.dto.VolumeDto;
 import com.example.book_app.repository.BookRepository;
 import com.example.book_app.repository.UserRepository;
 import com.example.book_app.service.BookService;
@@ -8,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class SearchController {
@@ -21,8 +27,21 @@ public class SearchController {
     @Autowired
     private BookRepository bookRepository;
 
+    //TODO: an idea for making the api results more accurate - replace the add book function entirely
+    // and make this \/ search for books by title AND author
+
+    //TODO: do this BEFORE making the add book function on the search results page
+
     @GetMapping("/mainPage/search/{title}")
     public String getSearchResults(@PathVariable String title, Model model) {
+        ApiResultsDto resultsDto = bookService.getApiResultsForBookByTitle(title);
+        List<VolumeDto> itemsFromResults = resultsDto.getItems();
+        List<BookAndUsersDto> booksResult = new ArrayList<>();
+        for (VolumeDto volumeDto : itemsFromResults) {
+            booksResult.add(volumeDto.getVolumeInfo());
+        }
+        model.addAttribute("results", booksResult);
         return "search";
     }
+
 }
